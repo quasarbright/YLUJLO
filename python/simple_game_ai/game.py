a8a038c831cf6dd75c2a71a6251b5109ea4917b0
Maggie Van Nortwick <maggievannortwick@yahoo.com>
2019-04-21 22:23:32 -0400

    Update game.py

--- python/simple_game_ai/game.py
+++ python/simple_game_ai/game.py
@@ -1,4 +1,4 @@
-class GameWorld:
+class Game:
     def __init__(self, width=3, height=3):
         self.width = width
         self.height = height
@@ -11,40 +11,58 @@ class GameWorld:
         self.target_y = 0
 
     def return_state(self):
-        return self.board
+        flat = []
+        for row in self.board:
+            for x in row:
+                flat.append(x)
+
+        return flat
 
     def move_player(self, direction):
         distance = abs(self.target_x - self.player_x) + abs(self.target_x - self.player_y)
 
-        # left
-        if direction == 0 and self.player_x != 0:
-            self.board[self.player_y][self.player_x] = 0
-            self.player_x -= 1
-            self.board[self.player_y][self.player_x] = 1
-
-        # up
-        if direction == 1 and player_y != 0:
-            self.board[self.player_y][self.player_x] = 0
-            self.player_y -= 1
-            self.board[self.player_y][self.player_x] = 1
-
         # right
-        if direction == 2 and player_x != width - 1:
+        if direction == 0 and self.player_x != self.width - 1:
             self.board[self.player_y][self.player_x] = 0
             self.player_x += 1
             self.board[self.player_y][self.player_x] = 1
 
+        # up
+        if direction == 1 and self.player_y != 0:
+            self.board[self.player_y][self.player_x] = 0
+            self.player_y -= 1
+            self.board[self.player_y][self.player_x] = 1
+
+        # left
+        if direction == 2 and self.player_x != 0:
+            self.board[self.player_y][self.player_x] = 0
+            self.player_x -= 1
+            self.board[self.player_y][self.player_x] = 1
+
         # down
-        if direction == 3 and player_y != height - 1:
+        if direction == 3 and self.player_y != self.height - 1:
              self.board[self.player_y][self.player_x] = 0
              self.player_y += 1
              self.board[self.player_y][self.player_x] = 1
 
         new_distance = abs(self.target_x - self.player_x) + abs(self.target_x - self.player_y)
-        reward = new_distance - distance
+        reward = distance - new_distance
 
-        return reward, return_state(self), check_target(self)
+        print(reward, self.return_state(), self.check_target())
+        return reward, self.return_state(), self.check_target()
 
+    def check_target(self):
+        return self.player_x == self.target_x and self.player_y == self.target_y
 
-    def check_targert(self):
-        return playerX == targetX and playerY == targetY
+game = Game()
+# right, then left
+game.move_player(0)
+game.move_player(2)
+# up, then down
+game.move_player(1)
+game.move_player(3)
+# win: up x3, right x3
+game.move_player(1)
+game.move_player(1)
+game.move_player(0)
+game.move_player(0)
