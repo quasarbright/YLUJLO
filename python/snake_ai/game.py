fd721d8ef1e4e1c2965275f5c57d132188d9cd36
quasarbright <mthundershock@gmail.com>
2019-04-22 20:05:12 -0400

    game skeleton

---
+++ python/snake_ai/game.py
@@ -0,0 +1,23 @@
+from graphics import *
+class Game:
+    def __init__(self, width, height, show=False):
+        if show:
+            pass
+        else:
+            pass
+        pass
+
+    def return_state(self):
+        '''
+        return representation as a 1D list
+        '''
+        pass
+    
+    def move_player(self, direction):
+        '''
+        direction is 0, 1, 2, or 3
+        '''
+        pass
+    
+    def render(self):
+        pass
