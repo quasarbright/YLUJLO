18fa465fb215f93bb951d34339018b3722390519
quasarbright <mthundershock@gmail.com>
2019-04-22 20:50:07 -0400

    add vecs, reverse tail, in bounds, start visible game

--- python/snake_ai/game.py
+++ python/snake_ai/game.py
@@ -11,33 +11,34 @@ class Vector:
     
     def __hash__(self):
         return hash((self.x, self.y))
+    
+    def __add__(self, other):
+        if type(self) is not type(other):
+            raise TypeError(str(type(other)))
+        return Vector(self.x + other.x, self.y + other.y)
 
 
 class Game:
-    def __init__(self, width, height, show=False):
-        if show:
-            pass
-        else:
-            pass
+    def __init__(self, width, height):
         self.width = width
         self.height = height
         startingHead = Vector(random.randint(0,width-1), random.randint(0, height-1))
-        self.tail = [startingHead] # list of vectors
+        self.tail = [startingHead] # list of vectors, from tail to head
         self.tailLength = len(self.tail)
         self.fruitPos = Vector()
         self.spawn_fruit()
-        self.direction = 0
+        self.direction = 1 # can be 1, 2, 3, or 4
     
     def spawn_fruit(self):
         self.fruitPos = Vector(random.randint(0, self.width-1), random.randint(0, self.height-1))
-        while self.fruitPos == self.tail[0]:
+        while self.fruitPos == self.tail[-1]:
             self.fruitPos = Vector(random.randint(0, self.width-1), random.randint(0, self.height-1))
     
     def is_in_bounds(self, p):
         '''
         is vector p in bounds?
         '''
-        pass
+        return p.x >=0 and p.x < self.width and p.y >= 0 and p.y < self.height
 
 
     def return_state(self):
@@ -52,6 +53,32 @@ class Game:
         nothing, right, up, left, down
         '''
         pass
+
+
+class VisibleGame(Game):
+    '''
+    a game that is rendered on screen
+    '''
+    def __init__(self, *args, **kwargs):
+        super(*args, **kwargs)
+        self.window_width = 500
+        self.window_height = 500
+        self.win = GraphWin("Snake", self.window_width, self.window_height)
+        self.rect_width = self.window_width / self.width
+        self.rect_height = self.window_height / self.height
     
-    def render(self):
-        pass
+    def background(self):
+        b = Rectangle(Point(0,0), Point(self.window_width, self.window_height))
+        b.setFill('gray')
+        b.draw(self.win)
+        for r in range(self.height):
+            for c in range(self.width):
+                p1 = Point(c*self.rect_width, r*self.rect_height)
+                p1 = Point((c+1)*self.rect_width, (r+1)*self.rect_height)
+                
+
+    
+    def close(self):
+        self.win.close()
+    
+
