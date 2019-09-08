e5172cac2440637aa924a130b7d432644e69c1b0
quasarbright <mthundershock@gmail.com>
2019-09-08 00:28:16 -0400

    left off about to do separation

---
+++ unity/Boids/Assets/Scripts/BoidfriendBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BoidfriendBehavior : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(1, 1, 0);
+    }
+
+    void FixedUpdate()
+    {
+        // move forward
+        // look at where we're going
+        if(rb.velocity.sqrMagnitude > 0f)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+        }
+    }
+}
