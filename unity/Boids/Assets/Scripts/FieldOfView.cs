98ccd02dc4c9a7ea43ea7d2f60df2010a3eb0875
quasarbright <mthundershock@gmail.com>
2019-09-13 20:18:21 -0400

    brack

---
+++ unity/Boids/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class FieldOfView : MonoBehaviour
+{
+    public GameObject population;
+    public float radius = 10f;
+    Rigidbody rb;
+    GameObject[] boids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        PopulationBehavior popBehavior = population.GetComponent<PopulationBehavior>();
+        boids = popBehavior.boids;
+    }
+
+    bool IsVisible(GameObject boid)
+    {
+        Vector3 pos = boid.transform.position;
+        Vector3 disp = 
+        Vector3 vel = rb.velocity;
+        float dot = Vector3.Dot(vel, )
+    }
+
+    public List<GameObject> GetVisibleBoids()
+    {
+        List<GameObject> ans = new List<GameObject>();
+        foreach (GameObject boid in boids)
+        {
+            
+        }
+        return ans;
+    }
+}
---
+++ unity/Boids/Assets/Scripts/PopulationBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationBehavior : MonoBehaviour
+{
+    public GameObject boidPrefab;
+    public int popSize = 100;
+    public GameObject boundingCube;
+    
+    [HideInInspector]
+    public GameObject[] boids;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boids = new GameObject[popSize];
+        for (int i = 0; i < popSize; i++)
+        {
+            Vector3 pos = SpawnPoint();
+            GameObject boidObj = Instantiate(boidPrefab, pos, transform.rotation);
+        }
+    }
+
+    Vector3 SpawnPoint()
+    {
+        float xmin, xmax, ymin, ymax, zmin, zmax;
+        Vector3 pos = boundingCube.transform.position;
+        float length = boundingCube.transform.lossyScale.x;
+        float width = boundingCube.transform.lossyScale.z;
+        float height = boundingCube.transform.lossyScale.y;
+        xmin = pos.x - length / 2f;
+        xmax = pos.x - length / 2f;
+        zmin = pos.z - width / 2f;
+        zmax = pos.z - width / 2f;
+        ymin = pos.y - height / 2f;
+        ymax = pos.y - height / 2f;
+
+        return new Vector3(Random.Range(xmin, xmax), Random.Range(ymin, ymax), Random.Range(zmin, zmax));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
