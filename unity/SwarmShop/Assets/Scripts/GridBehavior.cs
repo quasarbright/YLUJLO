534b512137eefa13c32a41913fbac64327542b9a
quasarbright <mthundershock@gmail.com>
2019-09-21 20:56:43 -0400

    started

---
+++ unity/SwarmShop/Assets/Scripts/GridBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehavior : MonoBehaviour
+{
+    public int length = 10;
+    public int height = 10;
+
+    private GameObject[,] objects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objects = new GameObject[height, length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ unity/SwarmShop/Assets/Scripts/OnGrid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGrid : MonoBehaviour
+{
+    private GridBehavior grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(transform.parent != null);
+        GameObject parent = transform.parent.gameObject;
+        grid = parent.GetComponent<GridBehavior>();
+        Debug.Assert(grid != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
